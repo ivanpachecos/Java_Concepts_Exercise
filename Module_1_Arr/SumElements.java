602fbcef0960ec0a06f8e024e4adbdeeec0935db
ivanpachecos <ivanpachecos@yahoo.com>
2024-08-02 00:58:47 -0500

    FIrt exercise

--- Module_1_Arr/SumElements.java
+++ Module_1_Arr/SumElements.java
@@ -3,7 +3,7 @@ package Module_1_Arr;
 class SumElements {
     public static void main(String[] args) {
         /*
-         * Description: Create a program that initializes an 
+         * Description: Create a program that initializes an
          * array with 10 integers and calculates the sum of all elements.
          */
         int[] numbers = {23, 1, 2, 3, 4, 5};
