fab27cbc8993660425fe963cf5a45754a499b193
ivanpachecos <ivanpachecos@yahoo.com>
2024-08-06 00:28:55 -0500

    sum of all array

--- Module_1_Arr/SumArr.java
+++ Module_1_Arr/SumArr.java
@@ -1,2 +1,14 @@
-package Module_1_Arr;public class SumArr {
+package Module_1_Arr;
+
+public class SumArr {
+    public static void main(String[] args) {
+        int[] numbers = {3, 3, 5, 1, 7, 4};
+        int totalNumber = 0;
+
+        for (int number : numbers) {
+            totalNumber += number;
+        }
+
+        System.out.println(totalNumber);
+    }
 }
